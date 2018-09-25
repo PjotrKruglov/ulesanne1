87a009acbf4c2a6e73adb1a48389be95bae44708
Pjotr Kruglov <t166038@vk.edu.ee>
2018-09-25 11:38:30 +0300

    button

--- index.php
+++ index.php
@@ -13,26 +13,44 @@
 <form action="result.php" method="get">
     <div class="form-group">
         <label for="usr">Name:</label>
-        <input type="text" class="form-control" id="usr">
+        <input name="username" type="text" class="form-control" id="usr">
     </div>
+
     <div class="form-group">
         <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd">
+        <input name="password" type="password" class="form-control" id="pwd">
+    </div>
+
     <div class="form-group">
         <label for="pwdc">Confirm password:</label>
-        <input type="password" class="form-control" id="pwdc">
+        <input name="password1" type="password" class="form-control" id="pwdc">
+    </div>
+
     <div class="form-check-inline">
         <label class="form-check-label">
         <input type="radio" class="form-check-input" name="optradio">Mees
         </label>
     </div>
+
     <div class="form-check-inline">
         <label class="form-check-label">
         <input type="radio" class="form-check-input" name="optradio">Naine
     </label>
+    </div>
 
-</div>
-</div> 
+    <div class="form-group">
+        <label for="tel">Phone:</label>
+        <input name="phone" type="text" class="form-control" id="tel">
+    </div>
+
+    <div class="form-group">
+        <label for="mail">E-mail:</label>
+        <input name="email" type="text" class="form-control" id="mail">
+    </div>
+
+    <button type="button" class="btn btn-primary btn-md" onclick="form.sumbit();">Confirm</button>
+
+    </form>
 </div>
 </body>
 </html>
