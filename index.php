fdf09c1f6c22ef715fd23b13a450dd5c342b0d4b
Pjotr Kruglov <t166038@vk.edu.ee>
2018-09-25 11:17:22 +0300

    test

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Registreerimisvorm</h1>
+<form action="result.php" method="get">
+    <div class="form-group">
+        <label for="usr">Name:</label>
+        <input type="text" class="form-control" id="usr">
+    </div>
+    <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd">
+    <div class="form-group">
+        <label for="pwdc">Confirm password:</label>
+        <input type="password" class="form-control" id="pwdc">
+    <div class="form-check-inline">
+        <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optradio">Mees
+        </label>
+    </div>
+    <div class="form-check-inline">
+        <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optradio">Naine
+    </label>
+
+</div>
+</div> 
+</body>
+</html>
